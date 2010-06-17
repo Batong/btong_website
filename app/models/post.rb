ff330f9cf01745450b997756c426c7b7b19b3afe
Batong <mattiaskarlss@gmail.com>
2010-06-17 14:40:04 +0200

    second commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,9 @@ class Post < ActiveRecord::Base
   validates_presence_of :name, :title
   validates_length_of :name, :minimum => 4
   validates_length_of :title, :minimum => 5
-  has_many :comments
+  has_many :comments #Post is connected to comments table
+  has_many :tags #Post is connected to tags table
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true  ,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
