5ee469a419492578caaebe6108a99a7b1fb3e52a
unknown <karlsmw1@LT-LN79527.worldwide.bbc.co.uk>
2014-02-01 19:42:02 +0000

    Add all the files to delete for my git directory as they are not being used anymore

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  validates_format_of :mail, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  
-   acts_as_authentic do |c|
-   c.login_field = :mail          # email is the login field
-   c. validate_login_field = false # There is no login field, so don't validate it
-   end
-end
